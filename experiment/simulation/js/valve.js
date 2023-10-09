59569f5b66fdc2b45fde5d70fe921df942c69ef3
thakuratul2 <pratapsinghatul111@gmail.com>
2023-10-09 20:18:47 +0530

    Simulation 7

--- experiment/simulation/js/valve.js
+++ experiment/simulation/js/valve.js
@@ -1,11 +1,11 @@
 
-const photo = document.getElementById("photo");
+const photo1 = document.getElementById("photo");
 const video = document.getElementById("video");
 const showVideoButton = document.getElementById("showVideoButton");
 
 showVideoButton.addEventListener("click", function () {
     // Hide the photo
-    photo.style.display = "none";
+    photo1.style.display = "none";
     // Show the video
     video.style.display = "block";
     // Start playing the video
